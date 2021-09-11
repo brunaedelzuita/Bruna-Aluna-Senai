6d14e1d34af340cbfae78007b2baa8fd2d7444d7
brunaedelzuita <bruesantos@gmail.com>
2021-09-11 16:52:39 -0300

    brunaedelzuita

---
+++ brunaalunasenai.js
@@ -0,0 +1,33 @@
+/*var media =7;
+if (media)=7;{
+    console.log("aprovado");
+}*/
+
+/*var media=4;;
+if(media)=7){
+    console.log("console.log("aprovado");
+}else{
+    console.log("Reprovado");
+
+    var media=4;
+    var faltas=1;
+    if(media>=7){
+        console.log("aprovado");
+    }else if (media>=5&&faltas<2){
+        console.log("recuperação");
+    }else{
+        console.log("reprovado");*/
+
+    for (let numero = 1; numero <=10; numero ++)
+        console.log(numero)
+
+        let x = 3;
+        const y = x++;
+        console.log(`x:${x}, y:${y}`)
+
+
+    
+    
+    
+
+
